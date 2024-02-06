a0758f4782ef731ccc44401e47950ffe4ec19248
Carson <104383295+codebycarson@users.noreply.github.com>
2024-02-06 15:35:11 -0700

    Hosting/testing error fixes
    - Don't auto create project config table
    - Changed import of verifyADR36Amino signature
    - return xAccountId on API response

--- src/schema/project.ts
+++ src/schema/project.ts
@@ -4,47 +4,35 @@ export const PROJECT_CONFIG_TABLE_NAME = 'engagemint-project_configuration_table
 
 export const ProjectConfigTickerKey = 'ticker';
 export const AdminWalletAddressKey = 'admin_wallet_address';
+
 const EpochLengthDays = 'epoch_length_days';
 const EpochStartDateUTC = 'epoch_start_date_utc';
 const LikeMultiplier = 'like_multiplier';
 const QuoteMultiplier = 'quote_multiplier';
+const ReplyMultiplier = 'reply_multiplier';
 const RetweetMultiplier = 'retweet_multiplier';
 const VideoViewMultiplier = 'video_view_multiplier';
 const ViewMultiplier = 'view_multiplier';
 const AdminWalletAddress = 'admin_wallet_address';
 const PreDefinedTweetText = 'pre_defined_tweet_text';
 
-export const ProjectConfigModel = dynamoose.model(PROJECT_CONFIG_TABLE_NAME, {
-	[ProjectConfigTickerKey]: {
-		type: String,
-		hashKey: true
-	},
-	[EpochLengthDays]: Number,
-	[AdminWalletAddress]: String,
-	[EpochStartDateUTC]: String,
-	[LikeMultiplier]: Number,
-	[QuoteMultiplier]: Number,
-	[RetweetMultiplier]: Number,
-	[VideoViewMultiplier]: Number,
-	[ViewMultiplier]: Number,
-	[PreDefinedTweetText]: String
-});
-
-export const ProjectConfigModelWithAdminWallet = dynamoose.model(
+export const ProjectConfigModel = dynamoose.model(
 	PROJECT_CONFIG_TABLE_NAME,
 	{
 		[ProjectConfigTickerKey]: {
 			type: String,
 			hashKey: true
 		},
-		[AdminWalletAddressKey]: String,
 		[EpochLengthDays]: Number,
+		[AdminWalletAddress]: String,
 		[EpochStartDateUTC]: String,
 		[LikeMultiplier]: Number,
 		[QuoteMultiplier]: Number,
+		[ReplyMultiplier]: Number,
 		[RetweetMultiplier]: Number,
 		[VideoViewMultiplier]: Number,
-		[ViewMultiplier]: Number
+		[ViewMultiplier]: Number,
+		[PreDefinedTweetText]: String
 	},
 	{ create: false }
 );
